c52cebf2e44fe19a78f7e9296d31ca90636e202f
Vandy Thong <github@dev.vndy.xyz>
2023-05-01 15:43:03 -0700

    added menu bar

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import TopRatedMoviePage from './movie/TopRatedMoviePage';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MovieDetailsPage from './movie/MovieDetailsPage';
+import DrawerAppBar from './MenuBar';
 
 
 function App() {
 
   return (
     <Router>
+      <DrawerAppBar />
       <Routes>
         <Route path="/" element={<TopRatedMoviePage />} />
         <Route path="/movie/:id" element={<MovieDetailsPage />} />
