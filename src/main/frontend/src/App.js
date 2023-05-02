7cba76ba91c7670a9f273f87e68073b1ddf81dbe
Vandy Thong <github@dev.vndy.xyz>
2023-05-02 10:03:14 -0700

    Add pagination for all movies to the movies app bar

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MovieDetailsPage from './movie/MovieDetailsPage';
 import DrawerAppBar from './MenuBar';
 import StarDetailsPage from './movie/StarDetailsPage';
+import MovieList from './movie/MovieList';
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route path="/" element={<TopRatedMoviePage />} />
         <Route path="/movie/:id" element={<MovieDetailsPage />} />
         <Route path="/star/:id" element={<StarDetailsPage />} />
+        <Route path="/movies" element={<MovieList />} />
       </Routes>
     </Router>
   );
